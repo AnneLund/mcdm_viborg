e35ed2cd6640e9f254e43c328a919ac83e996c9f
AnneLundM <anne@mediacollege.dk>
2025-02-14 10:11:23 +0100

    ddd

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "mcd-viborg-om232.ondigitalocean.app",
+        target: "https://mcd-viborg-om232.ondigitalocean.app",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
