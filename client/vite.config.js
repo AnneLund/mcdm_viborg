5d36adaab246d7d059ef1b56196df212ff6cc60c
AnneLundM <anne@mediacollege.dk>
2025-02-14 10:04:58 +0100

    qsq

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "mcd-viborg-om232.ondigitalocean.app",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  define: {
+    "process.env": import.meta.env,
+  },
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
