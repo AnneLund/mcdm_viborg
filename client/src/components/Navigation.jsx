1976f6106955cff629543f4d39680736b8745b86
AnneLundM <anne@mediacollege.dk>
2025-02-26 08:33:21 +0100

    fontsize

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -15,14 +15,16 @@ export default Navigation;
 const NavContainer = styled.nav`
   display: flex;
   justify-content: center;
-  gap: 20px;
+  gap: 25px;
   padding: 1rem;
   background: linear-gradient(135deg, #2c3e50, #4ca1af);
   border-radius: 10px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  margin: 20px 0;
 
   a {
     color: white;
+    font-size: 25px;
 
     &:hover {
       text-decoration: underline;
