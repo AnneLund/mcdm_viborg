7ed1302a9800036e1334eed36067c486d5fa86d0
AnneLundM <anne@mediacollege.dk>
2025-03-05 15:01:22 +0100

    fff

--- client/vite.config.js
+++ client/vite.config.js
@@ -28,6 +28,11 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+      },
     }),
   ],
   build: {
